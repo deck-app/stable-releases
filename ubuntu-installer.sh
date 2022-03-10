349bddaf05f0222d8fd61a01c77bb62beb36073e
Subhadip N <subhadip@get-deck.com>
2022-03-11 01:22:38 +0530

    Update ubuntu-installer.sh

--- ubuntu-installer.sh
+++ ubuntu-installer.sh
@@ -2,16 +2,16 @@
 #Set up the required package
 echo "Running apt update, installing dependencies"
 sudo apt update
-pkgs='curl uidmap apt-transport-https ca-certificates gnupg lsb-release docker.io neofetch docker-compose'
+pkgs='curl uidmap apt-transport-https ca-certificates gnupg lsb-release docker.io docker-compose'
 if ! dpkg -s $pkgs >/dev/null 2>&1; then
   sudo apt-get install -y $pkgs
 fi
 pkgs='deck'
-version=$(curl https://get-deck.com/latest.version)
-arch=`dpkg --print-architecture`
+version='4.0.0'
+arch='amd64'
 if ! dpkg -s $pkgs >/dev/null 2>&1; then
-  wget https://github.com/sfx101/deck/releases/download/$version/DECK-$version-$arch.deb
-  sudo dpkg -i DECK-$version-$arch.deb
+  wget https://github.com/deck-app/stable-releases/releases/download/v4.0.0/DECK-$version-linux-$arch.deb
+  sudo dpkg -i DECK-$version-linux-$arch.deb
 fi
 export PATH=/home/$USER/bin:$PATH
 export DOCKER_HOST=unix:///run/user/$USER/docker.sock
