bfae6a92e777ad6763270e3ea232dce805dfcfd0
Naba Kumar Das <nabad600@gmail.com>
2022-03-10 08:06:52 +0530

    Remove override

--- debin-linux-install.sh
+++ debin-linux-install.sh
@@ -2,7 +2,7 @@
 #Set up the required package
 echo "Running apt update, installing dependencies"
 sudo apt update
-pkgs='curl uidmap apt-transport-https ca-certificates gnupg lsb-release docker.io neofetch docker-compose docker-ce docker-ce-cli containerd.io'
+pkgs='curl uidmap apt-transport-https ca-certificates gnupg lsb-release docker.io neofetch docker-compose'
 if ! dpkg -s $pkgs >/dev/null 2>&1; then
   sudo apt-get install -y $pkgs
 fi
@@ -23,9 +23,6 @@ echo "Staring docker";
 sudo loginctl enable-linger $(whoami)
 echo "sudo chmod 666 /var/run/docker.sock";
 sudo chmod 666 /var/run/docker.sock
-sudo wget https://raw.githubusercontent.com/deck-app/multipass-install/master/override.conf
-sudo mkdir -p /etc/systemd/system/docker.service.d
-sudo cp override.conf /etc/systemd/system/docker.service.d/override.conf
 compose=$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')
 sudo curl -L "https://github.com/docker/compose/releases/download/$compose/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
